e3ea00f0013216e21b703b7be9d8b446b2a5135b
Essawey <M.Abdelmaged@nu.edu.eg>
2023-10-21 13:19:25 +0300

    .

--- MyClass.py
+++ MyClass.py
@@ -16,17 +16,13 @@ class MyClass:
         else:
           print()
 
-# Creating an instance of the class
+
 MyClass_Obj = MyClass(value=10)
 
-# Calling the instance method
 MyClass_Obj.generate_fibonacci()
 
-# Accessing the class attribute
 print("Current Value:", MyClass_Obj.get_value())
 
-# Setting a new value
 MyClass_Obj.set_value(20)
 
-# Accessing the updated value
 print("Updated Value:", MyClass_Obj.get_value())
